f4aeaf8fc2e617265bed7836190f1ca58db767cc
Christopher De Vries <devries@idolstarastronomer.com>
2022-10-23 16:15:51 -0400

    Move utilities into a separate repository

--- get_scores.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -z "${AOC_SESSION}" ]; then
-  echo "Must set AOC_SESSION environment variable to the session string."
-  exit 1
-fi
-
-curl --cookie session=${AOC_SESSION} https://adventofcode.com/2022/leaderboard/private/view/534400.json > 534400.json
